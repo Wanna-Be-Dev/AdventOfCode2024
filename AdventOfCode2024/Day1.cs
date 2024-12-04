313a416df29ac6e472e22b37276d1f8d154c5902
Greg <russian4ever13@gmail.com>
2024-12-04 10:46:28 +0300

    Init commit

---
+++ AdventOfCode2024/Day1.cs
@@ -0,0 +1,82 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    class Day1
+    {
+        public static string path = @"C:\Users\grman\source\repos\AdventOfCode2024\AdventOfCode2024\Input\Day1.txt";
+        public static string[] ReadInput()
+        {
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("No file Found");
+                return null;
+            }
+            
+            string[] lines = File.ReadAllLines(path);
+
+            return lines;
+        }
+        static int First()
+        {
+            int count = 0;
+            var list1 = new List<int>();
+            var list2 = new List<int>();
+
+            string?[] lines = ReadInput();
+
+            if (lines?.Length <= 0)
+                return -1;
+
+            foreach(string line in lines)
+            {
+                var number = System.Text.RegularExpressions.Regex.Split(line, @"\s+");
+                list1.Add(Int32.Parse(number[0]));
+                list2.Add(Int32.Parse(number[1]));
+            }
+
+            var newlist1 = list1.OrderBy(x => x).ToArray();
+            var newlist2 = list2.OrderBy(x => x).ToArray();
+
+            for(int i = 0; i< newlist1.Count(); i++)
+            {
+                var a = newlist1[i] - newlist2[i];
+                count += Math.Abs(a);
+            }
+
+            return count;
+        }
+        static int Second() 
+        {
+            int count = 0;
+            var list1 = new List<int>();
+            var list2 = new List<int>();
+            string?[] lines = ReadInput();
+
+            if (lines?.Length <= 0)
+                return -1;
+
+            foreach (string line in lines)
+            {
+                var number = System.Text.RegularExpressions.Regex.Split(line, @"\s+");
+                list1.Add(Int32.Parse(number[0]));
+                list2.Add(Int32.Parse(number[1]));
+            }
+            
+            for (int i = 0; i < list1.Count(); i++)
+            {
+                int similar = 0;
+                for(int j = 0; j < list2.Count(); j++)
+                {
+                    if (list1[i] == list2[j])
+                        similar++;
+                }
+                count += list1[i] * similar;
+            }
+
+            return count;
+        }
+       
+        
+    }
+}
---
+++ AdventOfCode2024/Day2.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    class Day2
+    {
+        public static string path = @"C:\Users\grman\source\repos\AdventOfCode2024\AdventOfCode2024\Input\Day2.txt";
+        public static string[] ReadInput()
+        {
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("No file Found");
+                return null;
+            }
+
+            string[] lines = File.ReadAllLines(path);
+
+            return lines;
+        }
+        static int First()
+        {
+            int count = 0;
+            string?[] lines = ReadInput();
+
+            if (lines?.Length <= 0)
+                return -1;
+
+            foreach (string line in lines)
+            {
+                int correct = 0;
+
+                var numbers = line.Split(' ').Select(n => Convert.ToInt32(n)).ToList();
+                var sorted = line.Split(' ').Select(n => Convert.ToInt32(n)).OrderBy(n => n).ToList();
+                var sortedRev = line.Split(' ').Select(n => Convert.ToInt32(n)).OrderByDescending(n => n).ToList();
+
+                if(sorted.SequenceEqual(numbers)|| sortedRev.SequenceEqual(numbers))
+                {     
+                    for(int i = 0; i < numbers.Count - 1;i++)
+                    {
+                        var diff = Math.Abs(numbers[i] - numbers[i + 1]);
+
+                        if (diff <= 3 && diff != 0)
+                            correct++;
+                    }
+                    if (correct == numbers.Count - 1)
+                        count++;
+                }
+            }
+
+            return count;
+        }
+
+        static int Second()//TODO:FIX later govnocode
+        {
+            int count = 0;
+            string?[] lines = ReadInput();
+
+            if (lines?.Length <= 0)
+                return -1;
+
+            foreach (string line in lines)
+            {
+                int correct = 0;
+                var numbers = line.Split(' ').Select(n => Convert.ToInt32(n)).ToList();
+                for (int i = 0; i < numbers.Count - 1; i++)
+                {
+                    var diff = Math.Abs(numbers[i] - numbers[i + 1]);
+                    if (diff <= 3 && diff != 0)
+                        correct++;
+                }
+                if (correct == numbers.Count - 1)
+                {
+                    var Ascend = new List<int>(numbers);
+                    var Descend = new List<int>(numbers);
+                    Ascend.OrderBy(n => n);
+                    Descend.OrderByDescending(n => n);
+                    if (numbers.SequenceEqual(Ascend) || numbers.SequenceEqual(Descend))
+                    {
+                        count++;
+                        for (int x = 0; x < numbers.Count; x++)
+                            Console.Write(numbers[x] + " ");
+                        Console.WriteLine("    @");
+                        continue;
+                    }
+                }
+                else
+                { 
+                    for (int a = 0; a < numbers.Count; a++)
+                    {
+                        correct = 0;
+                        var variant = new List<int>(numbers);
+                        variant.Remove(variant[a]);
+                        for (int j = 0; j < variant.Count - 1; j++)
+                        {
+                            var diff = Math.Abs(variant[j] - variant[j + 1]);
+                            Console.Write(variant[j] + " ");
+                            if (diff <= 3 && diff != 0)
+                                correct++;
+                        }
+                        var Ascend1 = new List<int>(variant);
+                        var Descend1 = new List<int>(variant);
+                        Ascend1.OrderBy(n => n);
+                        Descend1.OrderByDescending(n => n);
+                        Console.Write(variant[variant.Count - 1]);
+                        if (variant.SequenceEqual(Ascend1) || variant.SequenceEqual(Descend1))
+                        {
+                            if (correct == variant.Count - 1)
+                            {
+
+
+
+                                    count++;
+                                    Console.WriteLine("    @");
+                                    break;
+                                
+                            }
+                            Console.WriteLine(" ");
+                        }
+                    }
+
+                }
+            }
+
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Second());
+        }
+
+    }
+}
